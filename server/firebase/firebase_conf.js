88cb8a2f4314d2a10d5d346124d4f1515236020c
TaylorC19 <96517792+TaylorC19@users.noreply.github.com>
2023-04-06 21:08:32 +0900

    feat: updated firebase production config

--- server/firebase/firebase_conf.js
+++ server/firebase/firebase_conf.js
@@ -16,6 +16,14 @@ module.exports = {
     }
   },
   production: { 
-    config: process.env.FIREBASE
+    config: {
+      apiKey: process.env.FB_API_KEY,
+      authDomain: process.env.FB_AUTHDOMAIN,
+      projectId: process.env.FB_PROJECTID,
+      storageBucket: process.env.FB_STORAGEBUCK,
+      messagingSenderId: process.env.FB_MESSAGINGSENDERID,
+      appId: process.env.FB_APPID,
+      measurementId: process.env.FB_MEASUREMENTID
+    }
   }
 }
