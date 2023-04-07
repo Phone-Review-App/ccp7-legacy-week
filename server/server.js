3d2c830c8d95c591ef9dcb0f5e5e5de25d6dbb78
TaylorC19 <96517792+TaylorC19@users.noreply.github.com>
2023-04-07 12:32:01 +0900

    feat: post request will now add new memories

--- server/server.js
+++ server/server.js
@@ -22,6 +22,29 @@ function setupServer() {
 
   app.post('/api/memory', async (req, res) => {
     // accepts a users post request for adding a new memory
+    const newMemory = req.body;
+
+    const userIdObj = await knex('users')
+      .select('id')
+      .where('UID', '=', newMemory.uid)
+      .first();
+
+    const userId = userIdObj['id']
+
+    const submissionObject = {
+      user_id: userId,
+      prefecture_id: newMemory.prefecture_id,
+      photo_key: newMemory.photo_key,
+      description: newMemory.description
+    }
+    try {
+      await knex('photos').insert(submissionObject)
+      res.send(true);
+      
+    } catch (error) {
+      res.send(false);
+    }
+
   });
 
   app.get('/api/memory/:prefectureId', async (req, res) => {
